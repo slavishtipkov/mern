592d655db8974bcfe9e57fb97c435afddf18096c
Slavi Shtipkov <slavishtipkov1993@gmail.com>
2019-07-28 02:43:08 +0300

    Added Express.js and /task/new route for adding task

---
+++ src/server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import { connectDb } from "./connectDb";
+
+const port = 7777;
+const app = express();
+
+app.listen(port, console.log("Server listening on port ... ", port));
+
+app.use(cors(), bodyParser.urlencoded({ extended: true }), bodyParser.json());
+
+export const addNewTask = async task => {
+  let db = await connectDb();
+  let collection = db.collection("tasks");
+  await collection.insertOne(task);
+};
+
+app.post("/task/new", async (req, res) => {
+  let task = req.body.task;
+  await addNewTask(task);
+  res.status(200).send();
+});
---
+++ src/server/server.spec.js
@@ -0,0 +1,6 @@
+import { addNewTask } from "./server";
+
+addNewTask({
+  name: "My test task",
+  id: "213212213"
+});
