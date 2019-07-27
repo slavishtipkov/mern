923fd4800f70df9e4df769d72eae4b3bd763613b
Slavi Shtipkov <slavishtipkov1993@gmail.com>
2019-07-27 23:35:51 +0300

    Added React as well as basic connection with Redux

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
-console.log("Hello world!");
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { store } from "./store";
+import { Main } from "./components/Main";
 
-console.log(store.getState());
+ReactDOM.render(<Main />, document.getElementById("app"));
