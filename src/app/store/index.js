8cefd8e5c7ff03a954eece710445615d71f760f9
Slavi Shtipkov <slavishtipkov1993@gmail.com>
2019-07-28 01:11:20 +0300

    Implemented Redux-Saga for async actions and side effects

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,10 +1,42 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
 import { defaultState } from "../../server/defaultState";
+import { taskCreationSaga } from "./sagas.mock.js";
+import * as mutations from "./mutations";
 
-export const store = createStore(function reducer(
-  state = defaultState,
-  action
-) {
-  return state;
-});
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = createStore(
+  combineReducers({
+    tasks(tasks = defaultState.tasks, action) {
+      switch (action.type) {
+        case mutations.CREATE_TASK:
+          return [
+            ...tasks,
+            {
+              id: action.taskId,
+              name: "New Task",
+              group: action.groupId,
+              owner: action.ownerId,
+              isComplete: false
+            }
+          ];
+      }
+      return tasks;
+    },
+    comments(comments = defaultState.comments) {
+      return comments;
+    },
+    groups(groups = defaultState.groups) {
+      return groups;
+    },
+    users(users = defaultState.users) {
+      return users;
+    }
+  }),
+  applyMiddleware(createLogger(), sagaMiddleware)
+);
+
+sagaMiddleware.run(taskCreationSaga);
