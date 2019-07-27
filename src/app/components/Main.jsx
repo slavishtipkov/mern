12d2a42a422601c2412d9dd039c6063d94dc797b
Slavi Shtipkov <slavishtipkov1993@gmail.com>
2019-07-27 23:45:58 +0300

    Added React-Router and simple link to dashboard

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { store } from "../store";
+import { history } from "../store/history";
 
 import { ConnectedDashboard } from "./Dashboard";
+import { ConnectedNavigation } from "./Navigation";
 
 export const Main = () => (
-  <Provider store={store}>
-    <ConnectedDashboard />
-  </Provider>
+  <Router history={history}>
+    <Provider store={store}>
+      <ConnectedNavigation />
+      <Route exact path="/dashboard" render={() => <ConnectedDashboard />} />
+    </Provider>
+  </Router>
 );
